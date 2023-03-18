6a6307afe6ff369f3812ba4fe980c7331cf883c1
Prosante <hulvey7@gmail.com>
2023-03-18 07:46:08 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a chair: %i byte(s)\n", sizeo
+f(char));
+	print("Size of an int: %i byte(s)\n", sizeo
+f(int));
+	printf("Size of a long int: %i byte(s)\n", s
+izeof(long int));
+	printf("Size of a long long int: %i byte(s)\
+n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", size
+of(float));
+	return (0);
+
